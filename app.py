1be41caabca02a40c5b8503e013a2f928dcded8b
Pablodo24 <ayoubtt170@gmail.com>
2025-03-11 07:33:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask import Flask, jsonify
 from sklearn.preprocessing import MinMaxScaler
 from flask import Flask
 app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('index.html')
